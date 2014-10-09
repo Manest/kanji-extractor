c062dd24ac4dbdc9e9c4a4382ae6b22407433d22
Yoann Augen <yoann.augen@gmail.com>
2014-10-09 14:58:41 -0300

    Add link to jisho.org

--- js/kanji.js
+++ js/kanji.js
@@ -1,15 +1,23 @@
 $(function () {
     var list = [];
-    $( "#text" ).change(function() {
+    $( "#text" ).bind("keyup change", function(e) {
         var str = $(this).val();
+        $("#result").html("");
         for(var i = 0 ; i < str.length ; ++i) {
             if(/^[\u4e00-\u9faf]+$/.test(str[i])) {
                 if (list.indexOf(str[i]) == -1) {
-                    list.push(str[i]); 
+                    list.push(str[i]);
                 }
             }
         }
-        console.log(list);
+        for(var i = 0 ; i < list.length ; ++i) {
+            $("#result").append($('<a target="_blank">').
+                                attr("href", "http://jisho.org/kanji/details/"+list[i]).
+                                html(list[i]));
+        }
+        $("#result").append($('<a target="_blank">').
+            attr("href", "http://jisho.org/kanji/details/"+list.toString().replace(/,/g, "")).html("Show all"));
+
     });
 
 });

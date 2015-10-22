ec1ef5144d4ca0d1ddfa0085db3628386cf3bdc9
Yoann Augen <yoann.augen@gmail.com>
2015-10-22 15:16:21 -0300

    Refactor code and add percentage

--- js/kanji.js
+++ js/kanji.js
@@ -1,45 +1,57 @@
 $(function () {
-    $( "#text" ).bind("keyup change", function(e) {
+    var result = $("#result");
+    var link_bloc = result.find(".links");
+    var ordered_bloc = result.find(".ordered");
+    var result_bloc = result.find("div");
+    var textarea = $("#text");
+
+    var extract = function () {
         var list = [];
-        var str = $(this).val();
+        var str = $("#text").val();
         var objs = {};
-        $("#result div").html("");
-        for(var i = 0 ; i < str.length ; ++i) {
+        result_bloc.html("");
+        for (var i = 0 ; i < str.length ; ++i) {
             if(/^[\u4e00-\u9faf]+$/.test(str[i])) {
                 if (objs[str[i]] == undefined) {
-                    //list.push(str[i]);
                     objs[str[i]] = 1;
                 } else {
                     objs[str[i]] = objs[str[i]] + 1;
                 }
             }
         }
-
-        for (var i in objs) {
-            list.push([i, objs[i]])
+        for (i in objs) {
+            if (objs.hasOwnProperty(i)) {
+                list.push([i, objs[i]]);
+            }
         }
-        $("#result .links").append(list.length+" Kanji found<br />");
+        link_bloc.html(list.length+" Kanji found<br />");
 
-        for(var i = 0 ; i < list.length ; ++i) {
-            $("#result .links").append($('<a target="_blank">').
+        for (i = 0 ; i < list.length ; ++i) {
+            link_bloc.append($('<a target="_blank">').
                 attr("href", "http://jisho.org/kanji/details/"+list[i][0]).
                 html(list[i][0]));
         }
         if (list.length > 0) {
-            $("#result .links").append($('<a target="_blank">').
+            link_bloc.append($('<a target="_blank">').
                 attr("href", "http://jisho.org/kanji/details/"+list.toString().replace(/,/g, "")).html("Show all"));
         }
-        $("#result .links").append("<hr />");
+        link_bloc.append("<hr />");
 
-        list.sort(function(a, b) {return b[1] - a[1]})
-        $("#result .links").append("Number of each kanji:");
+        list.sort(function(a, b) {return b[1] - a[1]});
+        ordered_bloc.html("Statistics : <br />");
+        ordered_bloc.append(list.length+" Kanji<br />");
 
-        for(var i = 0 ; i < list.length ; ++i) {
-            $("#result .ordered").append(list[i][1]+' - ');
-            $("#result .ordered").append($('<a target="_blank">').
+        for (i = 0 ; i < list.length ; ++i) {
+            ordered_bloc.append(list[i][1]+" - ");
+            ordered_bloc.append($('<a target="_blank">').
                 attr("href", "http://jisho.org/kanji/details/"+list[i][0]).
-                html(list[i][0]+"<br />"));
-        }
-    });
+                html(list[i][0]));
+            ordered_bloc.append('<small>('+Math.round((list[i][1] * 100) / list.length)+'%)</small><br />');
 
+        }
+    };
+    textarea.bind("keyup change", extract);
+    if (textarea.html() != "") {
+       extract();
+    }
 });
